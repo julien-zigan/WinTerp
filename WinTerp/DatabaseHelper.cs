6ada19502874181020fa8aaf163500fe51616627
Julien <zigan.julien@gmail.com>
2024-11-29 15:57:25 +0100

    implement setup of database file in DatabaseHelper.cs

---
+++ WinTerp/DatabaseHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+using Microsoft.VisualBasic.ApplicationServices;
+
+namespace WinTerp
+{
+    public static class DatabaseHelper
+    {
+        private const string DatabaseDir = "WinTerp"; 
+        private const string DatabaseFile = "UserData.db";
+        private const string FileSeparator = @"/";
+
+
+        public static void InitializeDatabase()
+        {
+            string PathToDatabaseFile =
+                Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+
+            if (!Directory.Exists(PathToDatabaseFile + FileSeparator + DatabaseDir))
+            {
+                Directory.CreateDirectory(PathToDatabaseFile + FileSeparator + DatabaseDir);
+            }
+
+            if (!File.Exists(DatabaseFile))
+            {
+                File.Create(PathToDatabaseFile 
+                    + FileSeparator 
+                    + DatabaseDir
+                    + FileSeparator
+                    + DatabaseFile);
+            }
+
+        }
+    }
+}
---
+++ WinTerp/Form1.cs
@@ -0,0 +1,10 @@
+namespace WinTerp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
